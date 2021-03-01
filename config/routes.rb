3c3a14e7e96afa0d70bc24cb9ce7164501736dfe
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-02-28 23:06:35 -0300

    feat: show articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "welcome", to: "home#index"
   get "articles/new", to: "articles#new"
+  get "articles/:id", to: "articles#show" 
   post "articles", to: "articles#create"
 end
