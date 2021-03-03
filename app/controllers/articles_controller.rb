9ba13a90a519a9f49e49e9e3185ecd32c388887e
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-03-03 00:33:13 -0300

    styles: refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,18 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, expect: [:new, :create]
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-  @article = Article.find(params[:id])
   @article.update(title: params[:article][:title],content: params[:article][:content])
     redirect_to @article
 end
 
   def new 
     @article = Article.new
-    @article.title = 'Demo' 
   end
 
   def create
@@ -23,9 +20,10 @@ end
     render json: @article 
   end
   def destroy
-  @article = Article.find(params[:id])
   @article.destroy
   redirect_to root_path
   end
-
+  def find_article
+    @article = Article.find(params[:id])
+  end
 end
