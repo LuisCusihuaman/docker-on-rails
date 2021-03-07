51945e441ea8f1758f4aefbade93621692213d82
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-03-07 14:54:30 -0300

    feat: user articles relation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,8 +21,8 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.create(title: params[:article][:title],content: params[:article][:content])
-    render json: @article 
+    @article = current_user.articles.create(title: params[:article][:title],content: params[:article][:content])
+    redirect_to @article
   end
   
   def destroy
