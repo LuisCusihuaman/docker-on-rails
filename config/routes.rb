ee7dda657436fd9d8fb845814c2a24db8f2f4aea
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-03-07 15:39:16 -0300

    refactor: used resource rest syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
 
-  get "articles/user/:user_id", to: "articles#from_author"
+  resources :articles do
+    get "user/:user_id", to: "articles#from_author", on: :collection
+  end
 
-
-  get "articles", to: "articles#index"
-  get "articles/new", to: "articles#new", as: :new_articles
-  get "articles/:id", to: "articles#show" 
-  get "articles/:id/edit", to: "articles#edit"
-  patch "articles/:id", to: "articles#update", as: :article 
-  post "articles", to: "articles#create"
-  delete "articles/:id", to: "articles#destroy"
+  # get "articles", to: "articles#index"
+  # get "articles/new", to: "articles#new", as: :new_articles
+  # get "articles/:id", to: "articles#show" 
+  # get "articles/:id/edit", to: "articles#edit"
+  # patch "articles/:id", to: "articles#update", as: :article 
+  # post "articles", to: "articles#create"
+  # delete "articles/:id", to: "articles#destroy"
 end
