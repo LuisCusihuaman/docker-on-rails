262734ef0acde0bf25db4d0a47dcdbd65b1b13a8
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-03-07 17:37:19 -0300

    feat(articles): delete relations with categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,10 @@ class Article < ApplicationRecord
     attr_accessor :category_elements
 
     def save_categories
+        return has_categories.destroy_all if category_elements.nil? || category_elements.empty?
+
+        has_categories.where.not(category_id: category_elements).destroy_all
+
         category_elements.each do | category_id |
             HasCategory.find_or_create_by(article: self, category_id: category_id)
         end
